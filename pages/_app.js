a2ec986e78df93e4146cce3175a557b2e0fc7e9c
kristin-wiseman <kristin.w.dev@gmail.com>
2022-07-05 12:31:42 -0500

    Started working on responsiveness for About Me section in index.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,8 +8,13 @@ import Layout from '../components/layout';
 
 export default function MyApp({Component, pageProps}) {
     return (
+        <>
+        <Head>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
         <Layout>
             <Component {...pageProps} />
         </Layout>
+        </>
     )
 }
